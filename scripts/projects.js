6064428e38099f4b779b452c30fdf19cf25e6631
Kunalv7 <kunalvishwakarma3110@gmail.com>
2026-05-25 01:52:40 +0530

    project page's project.js only created

---
+++ scripts/projects.js
@@ -0,0 +1,20 @@
+            // <div class="projects">
+            //     <div class="project-info">
+            //         <h3>01</h3>
+            //         <h4>FullStack Threads Clone</h4>
+            //         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, qui ipsam! Illum doloremque eaque eos numquam iusto facere! Necessitatibus, odit.</p>
+            //         <div class="tech-stack">
+            //             <span>mongoDB</span>
+            //             <span>mongoDB</span>
+  
+            //         </div>
+            //         <hr>
+            //         <div class="links">
+            //             <a href="">
+            //                 <i class="ph ph-arrow-right"></i>
+            //             </a>
+            //             <a href="">
+            //                 <i class="ph ph-github-logo"></i>
+            //             </a>
+            //         </div>
+            //     </div>
